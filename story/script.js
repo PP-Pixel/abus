7fa9e44c62920e6d4d2fda7cc18548f56dc7c753
PP Pixel <134684159+PP-Pixel@users.noreply.github.com>
2023-10-10 15:46:49 +0900

    1

--- story/script.js
+++ story/script.js
@@ -4,7 +4,7 @@ const url = "https://pp-pixel.github.io/abus/1/";
 // 10秒後に移動するイベントを割り当てる
 setTimeout(() => {
   window.location.href = url;
-}, 10000);
+}, 35000);
 
 // 続きを表示する
 const container = document.querySelector("#container");
@@ -17,15 +17,26 @@ function showMoreText() {
 // 続きを表示するイベントを割り当てる
 container.querySelector("#more").addEventListener("click", showMoreText);
 
-// 画像にホバーしたときに情報を表示する
-const img = document.querySelector("img");
-img.addEventListener("mouseover", () => {
-  const info = document.querySelector(".info-g");
-  info.style.opacity = 1;
+// // 画像にホバーしたときに情報を表示する
+// const img = document.querySelector("img");
+// img.addEventListener("mouseover", () => {
+//   const info = document.querySelector(".info-g");
+//   info.style.opacity = 1;
+// });
+
+// // 画像から離れたときに情報を非表示にする
+// img.addEventListener("mouseout", () => {
+//   const info = document.querySelector(".info-g");
+//   info.style.opacity = 0;
+// });
+
+const audio = new Audio('sound.mp3');
+audio.volume = 0.5; // 音量を50%に設定
+ 
+document.getElementById('b-audio').addEventListener('mouseenter', () => {
+  audio.play();
 });
 
-// 画像から離れたときに情報を非表示にする
-img.addEventListener("mouseout", () => {
-  const info = document.querySelector(".info-g");
-  info.style.opacity = 0;
+document.getElementById('info-audio').addEventListener('click', () => {
+  audio.play();
 });
